7b0b45c427b08264e27873525faf470c50a9b895
Jay Honnold <jayhonnold@gmail.com>
2022-05-26 07:19:02 -0700

    Pre Quant

--- src/quantitize.h
+++ src/quantitize.h
@@ -77,11 +77,11 @@ void writeLayer(FILE* file, Tape* tunable_values, float wgt_scaling, float bia_s
     writeMatrix<bia_type>(file, bia, bia_scaling);
 }
 
-void quantitize(const std::string& path, Network& network, float scalar_1 = 1.0f, float scalar_2 = 1.0f) {
+void quantitize(const std::string& path, Network& network, float scalar_1 = 16, float scalar_2 = 512) {
     FILE *f = fopen(path.c_str(), "wb");
 
-    writeLayer<float, float>(f, network.getLayers()[0]->getTunableParameters()[0], scalar_1, scalar_1, true);
-    writeLayer<float, float>(f, network.getLayers()[1]->getTunableParameters()[0], scalar_2, scalar_1 * scalar_2, false);
+    writeLayer<int16_t, int16_t>(f, network.getLayers()[0]->getTunableParameters()[0], scalar_1, scalar_1, true);
+    writeLayer<int16_t, int32_t>(f, network.getLayers()[1]->getTunableParameters()[0], scalar_2, scalar_1 * scalar_2, false);
 
     fclose(f);
 }
